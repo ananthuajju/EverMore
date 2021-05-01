f21120ea4892e215ce3bd822ca72213da1b5d2f5
vashappening <57487077+vashappening@users.noreply.github.com>
2021-05-01 17:21:09 +0530

    removed unnecassary code that was commented out

--- controllers/validatebody.js
+++ controllers/validatebody.js
@@ -5,7 +5,6 @@ const validation = function (reqBody) {
 	const schema = Joi.object({
 		name: Joi.string().required().min(4),
 		email: Joi.string().required().email().min(6),
-		// password: Joi.string().required().min(6),
 	});
 	const data = schema.validate(reqBody);
 	return data;
